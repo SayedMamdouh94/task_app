82fc3cfeca1304499e20533796f1ed1cfd44a8ad
Sayed Mamdouh <sayedmamdouh07@gmail.com>
2025-09-04 03:18:19 +0300

    feat: Add splash screen and integrate SVG logo
    - Implemented SplashScreen widget to serve as the initial screen of the app.
    - Added SVG logo asset for the splash screen.
    - Updated app routing to navigate to the splash screen.
    - Integrated ScreenUtil for responsive design.
    - Updated Podfile.lock and project configurations for CocoaPods integration.
    - Added necessary dependencies in pubspec.yaml for SVG support.
    - Updated iOS and macOS project files to include new frameworks and configurations.

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,10 +2,14 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_app/core/style/app_colors.dart';
+import 'package:task_app/features/splash/presentation/ui/splash_screen.dart';
 
 class AppRouter {
   PageRoute? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case '/':
+        return _getPageRoute(const SplashScreen());
+      // Add other routes here as needed
       default:
         return null;
     }
